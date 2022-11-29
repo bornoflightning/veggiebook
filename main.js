58b0620969410a7d036ffe1b8ce96f9bac26d198
Evan <evanhughes66@yahoo.com>
2022-11-28 21:09:59 -0700

    included zipcode distance package

--- main.js
+++ main.js
@@ -4,6 +4,8 @@ const session = require('express-session');
 const exphbs = require('express-handlebars');
 const routes = require('./controllers');
 const helpers = require('./utils/helpers');
+const zipCodeData = require('zipcode-city-distance');
+
 
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
