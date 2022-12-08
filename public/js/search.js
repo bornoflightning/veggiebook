4b76014852324caed00b0056afa83a1c396832c3
Evan <evanhughes66@yahoo.com>
2022-12-07 22:00:04 -0700

    search function

--- public/js/search.js
+++ public/js/search.js
@@ -1,17 +1,21 @@
 const searchFormHandler = async (event) => {
     event.preventDefault();
+    const zipcode = document.querySelector('#searchbar').value.trim();
+    setTimeout(() => {
+        document.location.replace(`/search`);
+    }, 1000);
+    const response = await fetch('/search', {
+        method: 'POST',
+        body:''
+    })
 
     // Collect values from the login form
-    const zipcode = document.querySelector('#searchbar').value.trim();
-    console.log(zipcode);
+    console.log(growerlist);
 
     if (zipcode) {
         //Need to adjust the route below based on site map. 
 
         // Need to adjust the route below based on site map. 
-        setTimeout(() => {
-            document.location.replace(`/searchResults/:${zipcode}`);
-        }, 1000);
     } else {
         alert(response.statusText);
     }
